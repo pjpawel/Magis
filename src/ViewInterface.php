555104b003bac282ae4eeb499013dbe3c38fa675
pjpawel <pawel.jan.podgorski@gmail.com>
2022-11-20 20:16:54 +0100

    Add function addService to interface

--- src/ViewInterface.php
+++ src/ViewInterface.php
@@ -4,5 +4,8 @@ namespace pjpawel\Magis;
 
 interface ViewInterface
 {
+
     public function render(string $template, array $params): string;
+
+    public function addService(string $name, object $object): void;
 }
